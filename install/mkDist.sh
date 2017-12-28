12a85cfb4c012e2fc20f6473f42b9236c0938fc4
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2017-12-28 19:40:53 +0100

    Update some dependencies

--- install/mkDist.sh
+++ install/mkDist.sh
@@ -10,13 +10,12 @@ $srcDir/node_modules/.bin/electron-packager $srcDir/ mutant \
 	--platform=linux \
 	--arch=${arch} \
 	--version=1.4.0 \
-	--prune \
 	--ignore=".gitignore" \
 	--ignore=".git" \
 	--ignore="Model.md" \
 	--version-string.FileDescription="Mutant" \
-	--version-string.FileVersion="0.1.0" \
-	--version-string.ProductVersion="0.1.0" \
+	--version-string.FileVersion="0.1.4" \
+	--version-string.ProductVersion="0.1.4" \
 	--version-string.ProductName="Mutant" \
-	--app-version="0.1.0" \
+	--app-version="0.1.4" \
 	--overwrite
