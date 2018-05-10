2edc5845a4c8049e978a5d719a2cce7ff4a8faf9
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2018-05-10 13:51:53 +0200

    Add multiple urls support in web search apps

--- apps/system/webSearchApp/githubSearch.js
+++ apps/system/webSearchApp/githubSearch.js
@@ -1,64 +1,65 @@
 /************** AUTO GENERATED ***************/
-		'use strict';
+    'use strict';
 
-		var _utils   = global.app.utils;
-		var _url 	 = 'https://www.github.com/search?q=';
-		// make a regex out of the name for searching strings
-		var _queryRegex = /^Github (.*)/i;
-		var _fn = function( exec, query ){
+    var _utils   = global.app.utils;
+    var _url   = 'https://www.github.com/search?q=';
+    // make a regex out of the name for searching strings
+    var _queryRegex = /^Github (.*)/i;
+    var _fn = function( exec, query ){
 
-			var search = null;
-			if( exp.regex ){
-				search = _utils.cleanQuery(exp.regex.filter(r=>r!==null), query);
-				if( search ){
-					query = search;
-				}
-			}
-			query = _url + (query.split(' ')).join('+');
-			global.app.utils.spawn( 'xdg-open', [query] );
-		
-		}
+      var search = null;
+      if( exp.regex ){
+        search = _utils.cleanQuery(exp.regex.filter(r => r !== null), query);
+        if( search ){
+          query = search;
+        }
+      }
+      // url can have various values
+      _url.split('|')
+          .map(str => str + (query.split(' ')).join('+'))
+          .forEach(q => _utils.spawn('xdg-open', [q]))
+    }
 
-		var exp = {
-			fn: _fn,
-			wrapper: {
-				name: 'Open search on Github',
-				text: 'Search whatever on Github',
-				exec: 'githubSearch',
-				icon: 'github.png',
-				url: _url,
-				type: '_web_app_'
-			},
-			regex: [ _queryRegex, null ]
-		}
+    var exp = {
+      fn: _fn,
+      wrapper: {
+        name: 'Open search on Github',
+        text: 'Search whatever on Github',
+        exec: 'githubSearch',
+        icon: 'github.png',
+        url: _url,
+        type: '_web_app_'
+      },
+      regex: [ _queryRegex, null ]
+    }
 
-		if( global.settings.get('shortcuts.githubSearch') ){
-			// Avoid bad regex
-			var r = global.settings.get('shortcuts.githubSearch').regex1;
-			if( r !== '_unset_' ){
-				// Set default regex (index 0) and name search (index 1),
-				// setting to null avoids default behaviour, 
-				// which goes to the name of the application
-				exp.regex = [ _queryRegex, r ];
-			}	
-		}
+    if( global.settings.get('shortcuts.githubSearch') ){
+      // Avoid bad regex
+      var r = global.settings.get('shortcuts.githubSearch').regex1;
+      if( r !== '_unset_' ){
+        // Set default regex (index 0) and name search (index 1),
+        // setting to null avoids default behaviour, 
+        // which goes to the name of the application
+        exp.regex = [ _queryRegex, r ];
+      } 
+    }
 
-		module.exports = {
-			getRegex: function(){
+    module.exports = {
+      getRegex: function(){
         return (exp.regex)?exp.regex:null;
-	    },
-	    getUserRegex: function(){
+      },
+      getUserRegex: function(){
         return (exp.regex && exp.regex.length > 1)?exp.regex[1]:null;
-	    },
-	    getStdRegex: function(){
+      },
+      getStdRegex: function(){
         return (exp.regex && exp.regex.length)?exp.regex[0]:null;
-	    },
-			getWrapper: function(){
-				return exp;
-			},
-			testQuery: function( query ){
-				// Search by name regexp and by user custom regex 
-				return exp.regex.filter(r =>{ return r instanceof RegExp }).some(r =>{ return r.test( query ) });
-			}
-		}
-	
+      },
+      getWrapper: function(){
+        return exp;
+      },
+      testQuery: function( query ){
+        // Search by name regexp and by user custom regex 
+        return exp.regex.filter(r =>{ return r instanceof RegExp }).some(r =>{ return r.test( query ) });
+      }
+    }
+  
--- apps/system/webSearchApp/tpl.js
+++ apps/system/webSearchApp/tpl.js
@@ -1,89 +1,90 @@
 'use strict'
 
 module.exports = function( app ){
-	/*
-		Optional: { icon }
-		Mandatory: {
-			name: Used for wrapper name and text string,
-			exec: Used for execution and shortcut pick 
-			url: Url used to launch the app
-		}
-	*/
-	if( !app.name || !app.exec || !app.url ){
-		return null
-	}
+  /*
+    Optional: { icon }
+    Mandatory: {
+      name: Used for wrapper name and text string,
+      exec: Used for execution and shortcut pick 
+      url: Url used to launch the app
+    }
+  */
+  if( !app.name || !app.exec || !app.url ){
+    return null
+  }
 
-	var name = app.name;
-	var exec = app.exec;
-	var url  = app.url;
-	var icon = app.icon || null;
+  var name = app.name;
+  var exec = app.exec;
+  var url  = app.url;
+  var icon = app.icon || null;
 
-	var tpl = `/************** AUTO GENERATED ***************/
-		'use strict';
+  var tpl = `/************** AUTO GENERATED ***************/
+    'use strict';
 
-		var _utils   = global.app.utils;
-		var _url 	 = '${url}';
-		// make a regex out of the name for searching strings
-		var _queryRegex = /^${name} (\.*)/i;
-		var _fn = function( exec, query ){
+    var _utils   = global.app.utils;
+    var _url   = '${url}';
+    // make a regex out of the name for searching strings
+    var _queryRegex = /^${name} (\.*)/i;
+    var _fn = function( exec, query ){
 
-			var search = null;
-			if( exp.regex ){
-				search = _utils.cleanQuery(exp.regex.filter(r => r !== null), query);
-				if( search ){
-					query = search;
-				}
-			}
-			query = _url + (query.split(' ')).join('+');
-			global.app.utils.spawn( 'xdg-open', [query] );
-		
-		}
+      var search = null;
+      if( exp.regex ){
+        search = _utils.cleanQuery(exp.regex.filter(r => r !== null), query);
+        if( search ){
+          query = search;
+        }
+      }
+      // url can have various values
+      _url.split('|')
+          .map(str => str + (query.split(' ')).join('+'))
+          .forEach(q => _utils.spawn('xdg-open', [q]))
+    }
 
-		var exp = {
-			fn: _fn,
-			wrapper: {
-				name: 'Open search on ${name}',
-				text: 'Search whatever on ${name}',
-				exec: '${exec}',
-				icon: '${icon}',
-				url: _url,
-				type: '_web_app_'
-			},
-			regex: [ _queryRegex, null ]
-		}
+    var exp = {
+      fn: _fn,
+      wrapper: {
+        name: 'Open search on ${name}',
+        text: 'Search whatever on ${name}',
+        exec: '${exec}',
+        icon: '${icon}',
+        url: _url,
+        type: '_web_app_'
+      },
+      regex: [ _queryRegex, null ]
+    }
 
-		if( global.settings.get('shortcuts.${exec}') ){
-			// Avoid bad regex
-			var r = global.settings.get('shortcuts.${exec}').regex1;
-			if( r !== '_unset_' ){
-				// Set default regex (index 0) and name search (index 1),
-				// setting to null avoids default behaviour, 
-				// which goes to the name of the application
-				exp.regex = [ _queryRegex, r ];
-			}	
-		}
+    if( global.settings.get('shortcuts.${exec}') ){
+      // Avoid bad regex
+      var r = global.settings.get('shortcuts.${exec}').regex1;
+      if( r !== '_unset_' ){
+        // Set default regex (index 0) and name search (index 1),
+        // setting to null avoids default behaviour, 
+        // which goes to the name of the application
+        exp.regex = [ _queryRegex, r ];
+      } 
+    }
 
-		module.exports = {
-			getRegex: function(){
+    module.exports = {
+      getRegex: function(){
         return (exp.regex)?exp.regex:null;
-	    },
-	    getUserRegex: function(){
+      },
+      getUserRegex: function(){
         return (exp.regex && exp.regex.length > 1)?exp.regex[1]:null;
-	    },
-	    getStdRegex: function(){
+      },
+      getStdRegex: function(){
         return (exp.regex && exp.regex.length)?exp.regex[0]:null;
-	    },
-			getWrapper: function(){
-				return exp;
-			},
-			testQuery: function( query ){
-				// Search by name regexp and by user custom regex 
-				return exp.regex.filter(r =>{ return r instanceof RegExp }).some(r =>{ return r.test( query ) });
-			}
-		}
-	`
+      },
+      getWrapper: function(){
+        return exp;
+      },
+      testQuery: function( query ){
+        // Search by name regexp and by user custom regex 
+        return exp.regex.filter(r =>{ return r instanceof RegExp }).some(r =>{ return r.test( query ) });
+      }
+    }
+  `
 
-	return tpl;
+  return tpl;
 
 }
 
