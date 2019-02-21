14a328ce5604253f42f2a19faebfbbd462e49577
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2019-02-21 22:26:17 +0100

    Improve automatic build

--- install/mkDist.sh
+++ install/mkDist.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 
-# Enter Working Dir (shall be project root)
-srcDir="$1"
+if [[ -d ./dist ]]; then
+  rm -rf ./dist
+fi
 
-[[ -d "$srcDir" ]] || exit 255;
+mkdir ./dist && cd ./dist
 
-arch=x64
-$srcDir/node_modules/.bin/electron-packager $srcDir/ mutant \
-	--platform=linux \
-	--arch=${arch} \
-	--version=1.4.0 \
-	--ignore=".gitignore" \
-	--ignore=".git" \
-	--ignore="Model.md" \
-	--version-string.FileDescription="Mutant" \
-	--version-string.FileVersion="0.1.4" \
-	--version-string.ProductVersion="0.1.4" \
-	--version-string.ProductName="Mutant" \
-	--app-version="0.1.4" \
-	--overwrite
+echo "[Desktop Entry]" > mutant.desktop
+echo "Encoding=UTF-8" >> mutant.desktop
+echo "Version=1.0" >> mutant.desktop
+echo "Name=mutant" >> mutant.desktop
+echo "Comment=Productivity Launcher" >> mutant.desktop
+echo "Exec=/opt/mutant/mutant" >> mutant.desktop
+echo "Terminal=false" >> mutant.desktop
+echo "Type=Application" >> mutant.desktop
+echo "Categories=Utility;" >> mutant.desktop
+
+# TODO copy icon to icons folder
+version=$(node -p "require('../package.json').version")
+
+../node_modules/electron-packager/cli.js ../ mutant \
+  --platform=linux \
+  --arch=x64 \
+  --electron-version=$(electron -v | cut -c 2-) \
+  --prune \
+  --ignore=".git" \
+  --ignore=".gitignore" \
+  --ignore="install" \
+  --ignore="Model.md" \
+  --version-string.FileDescription="Mutant" \
+  --version-string.FileVersion="$version" \
+  --version-string.ProductVersion="$version" \
+  --version-string.ProductName="mutant" \
+  --app-version="$version" \
+  --overwrite
+
+mv mutant-linux-x64 mutant
+
+cd ..
